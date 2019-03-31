b33d026010f0378fc4bd05810da7bba6013ef54f
diegoeso <diego.enrique76@gmail.com>
2019-03-31 16:44:06 -0600

    .env

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,9 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8"/>
-
         <meta content="{{ csrf_token() }}" name="csrf-token"/>
-        
         <meta content="IE=edge" http-equiv="X-UA-Compatible"/>
         <title>
             Space Manager
